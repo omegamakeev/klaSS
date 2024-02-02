a1f56ca05a61131b3551873ba37fcd56c6d93de9
denis <157492096+omegamakeev@users.noreply.github.com>
2024-02-02 11:23:31 +0300

    02.02.2024 классная работа 2 часть

---
+++ main (4).py
@@ -0,0 +1,36 @@
+ya = 1 
+ba = 10 
+if ya < ba and ya == 1: 
+    print("true")
+else:
+    print("false")
+if ba * 2 == 20 and ya * 2 == 2:
+    print("true")
+else: 
+    primt("false")
+if ya == 2 and ba * 10 == 15:
+    print("true")
+else: 
+    print("false")
+if ya * 10 == 24 and ba < ya:
+    print("true")
+else:
+    print("false")
+
+if ya < ba or ya == 2:
+    print("true")
+else:
+    print("false")
+if ya * 10 == 10 or ba / 5 == 3:
+    print("true")
+else:
+    print("false")
+if ya * 10 == 100 or ba * 100 == 100:
+    print("true")
+else: 
+    print("false")
+if ya > ba or ba / 1000 == 1:
+    print("true")
+else: 
+    print("false")
+
