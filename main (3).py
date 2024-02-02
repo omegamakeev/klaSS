74107a34e51feb3b747e62e19c483f9cc0fd6fef
denis <157492096+omegamakeev@users.noreply.github.com>
2024-02-02 11:06:37 +0300

    02.02.24 классная работа 5 заданий

---
+++ main (3).py
@@ -0,0 +1,16 @@
+tseloe = 10 
+drobnoe = 8.4 
+stroka = 'No'
+
+big_netseloe = tseloe * 3.5 
+
+drobnoe -= 1 
+
+a = tseloe / drobnoe 
+b = big_netseloe/ drobnoe 
+print(a, b)
+
+stroka = "No"*2+"_"+"Yes"*3
+
+print(tseloe, drobnoe, stroka, big_netseloe)
+
