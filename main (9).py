c08630e9a2c49282feb91a4e65c5ba94d87015a1
denis <157492096+omegamakeev@users.noreply.github.com>
2024-04-02 15:01:36 +0300

    02.04.2024 пример ф-ции

---
+++ main (9).py
@@ -0,0 +1,13 @@
+def schet (): 
+    a = input ("Введите первое число: ")
+    a = float (a)
+    b = input ("Введите второе число: ")
+    b = float (b)
+    if a > b: 
+        print(a-b)
+        else: 
+            print(b-a)
+        return a, b 
+c,d = schet ()
+e,f = schet ()
+z,x = schet ()
